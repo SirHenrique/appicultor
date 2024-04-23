9ed260965a9f66ae88f2dd5e975a7c9f7ab46ea2
SirHenrique <henriquenicolli123@gmail.com>
2024-04-23 20:58:30 -0300

    inserir botão de edição

--- src/screens/apiarios.tsx
+++ src/screens/apiarios.tsx
@@ -6,6 +6,7 @@ import Constants from 'expo-constants';
 import { FlatList, StatusBar } from 'react-native';
 import { AlignCenter } from '@tamagui/lucide-icons';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Ionicons } from '@expo/vector-icons';
 
 type ApiarioSreenRouteProp = RouteProp<RootStackParamList, 'Apiario'>;
 
@@ -17,7 +18,18 @@ const data = [
 export default function Apiario() {
    const renderItem = ({ item }) => (
       <TouchableOpacity style={{width:350, height:100, justifyContent:'center', borderRadius:10, paddingHorizontal:10, backgroundColor:'#F5E6C3'}}>
+        <View flexDirection='row' justifyContent='space-between'>
         <Text fontWeight={'bold'} fontSize={20} paddingBottom={20}>Apiário {item.id}</Text>
+        <View flexDirection='row'>
+        <TouchableOpacity style={{backgroundColor:'#FFBC00', marginRight:15, borderRadius:5, height:40, width:40, alignItems:'center', justifyContent:'center'}}>
+         <Ionicons name='pencil' size={30} color={'#fff'}/>
+        </TouchableOpacity>
+        <TouchableOpacity style={{backgroundColor:'#F11010',borderRadius:5, height:40, width:40,alignItems:'center', justifyContent:'center'}}>
+         <Ionicons name='trash' size={30} color={'#fff'}/>
+        </TouchableOpacity>
+        </View>
+
+        </View>
         <Text>Qtd. Colmeias: {item.qtdColmeias}</Text>
         <Text>Localização: {item.localizacao}</Text>
       </TouchableOpacity>
